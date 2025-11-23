d1a0b74b45bd1e5e8c9af0deb53abbf61870c05a
blu-sea <github-mph@mlpa.com.au>
2025-11-23 20:45:55 +0000

    Update build.sh - add konsole back into aurora image

--- build_files/build.sh
+++ build_files/build.sh
@@ -10,8 +10,9 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-dnf5 install -y tmux mc
-# this is a quick hack local rpm install, TODO - make the repo work instead so this rpm updates automatically
+dnf5 install -y tmux mc konsole
+
+# VPN this is a quick hack local rpm install, TODO - make the repo work instead so this rpm updates automatically
 dnf5 install -y /ctx/nordvpn-4.2.3-1.x86_64.rpm
 
 # Use a COPR Example:

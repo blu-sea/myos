04a9c7502634eaa9a77ba1b441ffc91501c361be
blu-sea <github-mph@mlpa.com.au>
2025-11-25 21:46:31 +0000

    Update build.sh - edit netbird repo

--- build_files/build.sh
+++ build_files/build.sh
@@ -38,7 +38,7 @@ baseurl=https://pkgs.netbird.io/yum/
 enabled=1
 gpgcheck=0
 gpgkey=https://pkgs.netbird.io/yum/repodata/repomd.xml.key
-repo_gpgcheck=1
+repo_gpgcheck=0
 EOF
 
 #dnf5 -y config-manager addrepo --from-repofile=/etc/yum.repos.d/netbird.repo
